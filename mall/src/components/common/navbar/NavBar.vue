e901ed5fe35589b66eba1c7aa45f302d168e5495
lemon <freesme@163.com>
2019-11-12 00:02:27 +0800

    nav-bar

---
+++ mall/src/components/common/navbar/NavBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="nav-bar">
+    <div class="left">
+      <slot name="left"></slot>
+    </div>
+    <div class="center">
+      <slot name="center"></slot>
+    </div>
+    <div class="right">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "NavBar"
+  }
+</script>
+
+
+
+<style scoped>
+  .nav-bar {
+    display: flex;
+    line-height: 44px;
+    height: 44px;
+    text-align: center;
+    box-shadow:0 1px 1px silver;
+  }
+  .left{
+    width: 60px;
+    /*background-color: blue;*/
+  }
+  .right{
+    width: 60px;
+    /*background-color: blue;*/
+  }
+  .center{
+    flex: 1;
+    /*background-color: red;*/
+  }
+
+</style>
--- mall/src/views/home/Home.vue
+++ mall/src/views/home/Home.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="home">
-    <h2>home</h2>
+    <nav-bar class="home-nav"><div slot="center">蘑菇街</div></nav-bar>
   </div>
 </template>
 
 <script>
-
+import NavBar from 'components/common/navbar/NavBar'
 export default {
 		name: "Home",
     components: {
-
+      NavBar
     }
 	}
 </script>
 
 <style scoped>
-
+  .home-nav{
+    background-color: #fd5777;
+    color: #f6f6f6;
+  }
 
 </style>
