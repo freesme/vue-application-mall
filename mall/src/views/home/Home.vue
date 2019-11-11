26a5bad826b54221533254969d343f57cc759138
lemon <freesme@163.com>
2019-11-12 00:55:32 +0800

    首页轮播,接口有点问题

--- mall/src/views/home/Home.vue
+++ mall/src/views/home/Home.vue
@@ -1,21 +1,44 @@
 <template>
   <div id="home">
-    <nav-bar class="home-nav"><div slot="center">蘑菇街</div></nav-bar>
+    <nav-bar class="home-nav">
+      <div slot="center">蘑菇街</div>
+    </nav-bar>
+    <home-swiper :banners="banners"
+                 ref="hSwiper"></home-swiper>
   </div>
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar'
-export default {
-		name: "Home",
+  import {getHomeMultidata} from "network/home";
+  import HomeSwiper from './childComps/HomeSwiper'
+  import NavBar from 'components/common/navbar/NavBar'
+
+  export default {
+    name: "Home",
     components: {
-      NavBar
+      NavBar,
+      HomeSwiper
+    },
+    data(){
+      return{
+        // result:null
+        banners:[],
+        recommeds:[]
+      }
+    },
+    created() {
+      getHomeMultidata().then(res=>{
+        console.log(res);
+        // this.result = res
+        this.banner = res.data.banner.list
+        this.recommeds = res.data.recommend.list
+      })
     }
-	}
+  }
 </script>
 
 <style scoped>
-  .home-nav{
+  .home-nav {
     background-color: #fd5777;
     color: #f6f6f6;
   }
